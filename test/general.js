841b0287c3f1f2c7d7c7a0fc54bac588976d978f
Benjamin Pannell <admin@sierrasoftworks.com>
2013-12-12 16:37:44 +0200

    Support wildcard permissions, v1.1.0

--- test/general.js
+++ test/general.js
@@ -28,6 +28,21 @@ it('should correctly process nested permissions', function() {
 	GuardHouse.can(permissions, 'account.delete').should.be.true;
 });
 
+it('should correctly process wildcard permissions', function() {
+	var permissions = {
+		api: {
+			admin: false,
+			'*': true
+		},
+		account: true
+	};
+
+	GuardHouse.can(permissions, 'api.admin').should.be.false;
+	GuardHouse.can(permissions, 'api.test').should.be.true;
+	GuardHouse.can(permissions, 'api.user').should.be.true;
+	GuardHouse.can(permissions, 'account.delete').should.be.true;
+});
+
 it('should correctly wrap functions', function(done) {
 	var permissions = {
 		f1: true,
