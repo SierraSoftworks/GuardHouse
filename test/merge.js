272b5570630a0849efad2579f178d23ae1534e61
Benjamin Pannell <admin@sierrasoftworks.com>
2013-12-27 15:12:24 +0200

    Added merge tests

---
+++ test/merge.js
@@ -0,0 +1,8 @@
+var guardhouse = require('../');
+var should = require('should');
+
+describe('permission merging', function() {
+	it('should correctly grant basic permissions', function() {
+		
+	});
+});
