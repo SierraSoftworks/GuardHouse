9b28f7671dd17712ae180ba48fea7569617c405d
Benjamin Pannell <admin@sierrasoftworks.com>
2014-06-24 12:30:49 +0200

    Added ghlite for client side permission checking

---
+++ dist/ghlite.js
@@ -0,0 +1,34 @@
+(function(window) {
+	var gh = window.GuardHouse = {
+		options: {
+			default: !!0
+		}
+	};
+
+	gh.PathProcessor = function(d) {
+		return function(p) {
+			p = p.split(d);
+			if(!p[0]) p.shift();
+			return p;
+		}
+	};
+
+	gh.options.targetProcessor = gh.PathProcessor('/');
+
+	gh.can = function(p,t) {
+		var s = gh.options.targetProcessor(t);
+		if(p === undefined || p === null) return gh.options.default;
+		for(var i = 0; i < s.length; i++) {
+			if(typeof p == 'boolean') return p;
+			if(!p.hasOwnProperty(s[i])) {
+				if(p.hasOwnProperty('*')) s[i] = '*';
+				else return gh.options.default;
+			}
+			p = p[s[i]];
+		}
+		if(typeof p == 'boolean') return p;
+		return gh.options.default;
+	};
+
+	return gh;
+})(window);
